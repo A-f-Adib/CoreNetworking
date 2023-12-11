7f1dd45f2bf6982f61f8afe563622687faa0f005
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-12-12 02:48:47 +0600

    Initial Commit

---
+++ SwiftNetworking/SwiftNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkingApp.swift
+//  SwiftNetworking
+//
+//  Created by A.f. Adib on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
